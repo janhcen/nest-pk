326327750a34f60046a19b05ddf90de62530307d
janhuaman <janhuaman@redteam.com>
2023-11-28 16:50:40 -0500

    v01

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      `https://pokeapi.co/api/v2/pokemon?limit=650`,
+    );
+
+    const pokemonsInsert = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      pokemonsInsert.push({ name, no });
+    });
+
+    await this.pokemonModel.insertMany(pokemonsInsert);
+
+    return 'Seed executed';
+  }
+}
