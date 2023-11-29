aa1ef8a8c42dd6c308ecb0332fb4ad4170c3b0c5
janhuaman <janhuaman@redteam.com>
2023-11-28 22:58:03 -0500

    v02

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -15,7 +15,7 @@ export class Pokemon extends Document {
     unique: true,
     index: true,
   })
-  no: string;
+  no: number;
 }
 
 export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
